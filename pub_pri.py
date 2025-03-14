1348b8d14b7cda1a4eb87e10f9287d82155c0ebd
Afreen-cpu <nusrathulla123@gmail.com>
2025-03-14 10:49:59 +0530

    Create pub_pri.py

---
+++ pub_pri.py
@@ -0,0 +1,9 @@
+class Affu
+   def_init_(self):
+    self.pub_var = "ok"
+    self._affu_var="not ok"
+    def sudha_private(self)
+        print(self._private_var) 
+ kavya=affu() 
+ print(Affu.pub_var)  
+     
